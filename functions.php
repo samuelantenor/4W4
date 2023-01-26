5a54521631aaae737ddb834e0ca064a20314c64d
Samuel Antenor <asamuelandre@gmail.com>
2023-01-26 11:57:54 -0500

    s1c2 amelioration du site word press (index.php,header,footer etc.)

--- functions.php
+++ functions.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 /**
- * L'ensemble des fonctions du thème 
+* L'ensemble des fonctions du thème
 */
+function enfiler_css() {
+wp_enqueue_style('4w4-gr1-principal', // id
+        get_template_directory_uri() . '/style.css', // adresse url de style.css
+        array(), // définir les dépendances
+        filemtime(get_template_directory() . '/style.css'), // le calcul de la version du fichier css
+        'all'); // media
+}        
 
-
-wp_enqueue_style('4w4-gr2-principal', // identificateur
-                                 get_template_directory_uri() . '/style.css', // adresse url de style.css
-                                 array(), // définir les dépendances
-                                 filemtime(get_template_directory() . 'style.css'), //calcule de la version du fichier css
-                                 ); // media 
-?>
+add_action( 'wp_enqueue_scripts', 'enfiler_css' );        
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mon 4W4</title>
-    <?php wp_head() ?>
-</head>
-<body>
-    <h1>Bienvenue sur 4w4</h1>
+<?php 
+/**
+ * index.php est le modèle par défaut du thème 4w4 
+ */
+?>
+
+<?php get_header(); ?>
+    <main>
+        <h1>Bienvenue sur 4W4</h1>
+    </main>
+    
+    <?php get_footer(); ?>
 </body>
 </html>
