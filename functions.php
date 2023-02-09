10c8de541d4881c215afb17f1eae057ddf468488
Samuel Antenor <asamuelandre@gmail.com>
2023-02-09 10:34:48 -0500

    s3c2 ajout d'un menu et modification css

--- functions.php
+++ functions.php
@@ -11,3 +11,21 @@ wp_enqueue_style('4w4-gr1-principal', // id
 }        
 
 add_action( 'wp_enqueue_scripts', 'enfiler_css' );
+
+/* ------------------------------------------------------------------------------------------------------- ENREGISTRMENT DES MENUS*/
+if ( ! function_exists( 'enregistre_menus' ) ) {
+
+	function enregistre_menus(){
+		register_nav_menus( array(
+	    	'menu_entete' =>  'Menu entete',
+	    	'menu_sidebar'  => 'Footer Menu' ,
+		) );
+	}
+	add_action( 'after_setup_theme', 'enregistre_menus', 0 );
+}
+/* ------------------------------------------------------------------------------------------------------- add_theme_support*/
+add_theme_support( 'title-tag' );
+add_theme_support( 'custom-logo', array(
+    'height' => 150,
+    'width'  => 150 ,
+) );
