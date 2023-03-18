da03e48cc6e05f56442a73a66a0a40f54a7bd857
Samuel Antenor <asamuelandre@gmail.com>
2023-03-17 20:50:25 -0400

    Amelioration du footer

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <footer class="site__footer">
-    4W4-Samuel A.
-    <?php wp_nav_menu(array(
-                    "menu"=>"entete",
-                    "container" => "nav",
-                    "container_class" => "menu__entete"
-        ));          
-        ?>
+<?php the_custom_logo() ?>
+
+<?php wp_nav_menu(array(
+    "menu"=>"entete",
+    "container"=>"nav",
+    "container_class" => "menu__entete"
+     ))?>
+<section>Samuel Antenor</section>
+<section> 4W4 - Conception d'interface et développement web</section>
+
 </footer>
-        <?php wp_footer(); ?>
-        
-</body>
-</html>    
+<?php wp_footer(); ?>
