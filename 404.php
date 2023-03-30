b73432999977c00512eb9ae8b421f8791f01be6c
Samuel Antenor <asamuelandre@gmail.com>
2023-03-30 09:56:04 -0400

    intégration de la mise en page erreur 404

--- 404.php
+++ 404.php
@@ -1,12 +1,37 @@
 <?php 
 /**
- * index.php est le modèle par défaut du thème 4w4 
+ * 404.php is the template for displaying 404 pages.
  */
-?>
 
-<?php get_header(); ?>
-    <main class="site__main">
-        <pre>404.php</pre>
-        <h1>Erreur 404</h1>
-    </main>
-    <?php get_footer(); ?>
+get_header(); ?>
+
+<main class="site__main">
+
+  <div class="error-404">
+
+    <h1>Error 404</h1>
+    <h2>Page not found, you can try searching.</h2>
+
+    <div class="menu-cours">
+      <?php
+        wp_nav_menu( array(
+          'theme_location' => 'menu-cours',
+          'menu_class'     => 'menu'
+        ) );
+      ?>
+    </div>
+
+    <div class="menu-note-4w4">
+      <?php
+        wp_nav_menu( array(
+          'theme_location' => 'menu-note-4w4',
+          'menu_class'     => 'menu'
+        ) );
+      ?>
+    </div>
+
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
