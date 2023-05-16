edda1f575055b5e6b81aecbefa3d4639dfe18050
Samuel Antenor <asamuelandre@gmail.com>
2023-05-16 14:30:12 -0400

    Rajout d'un background image et améliration du aside

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -1,23 +1,18 @@
-<?php
+<?php 
 /**
- * Template permettant d'afficher la zone aside
- */
-?>
-<aside class="site__aside">
-    <h3>Menu secondaire</h3>
-    <?php 
-    $menu = "note-4w4"; 
-    if(in_category('cours')){ $menu = "cours";}
-      // $menu peut prendre les valeurs : "note-4w4" ou "cours"
-        echo $menu;
-        wp_nav_menu(array(
-        "menu"=>$menu,
-        "container"=>"nav"
-    )) ?>
+ * Template permettant de montrer l'aside */ ?>
 
-<section>
-    <div class="sidebar">
-    <?php  dynamic_sidebar('footer_2'); ?>  
-    </div>
-</section>
-</aside>
+<aside class="site__aside">
+    
+        <h3>Menu</h3>
+       <?php 
+            $menu = "cour";
+            if(in_category('Cours')){ $menu='Cours';}
+            // $menu peut prendre les valeurs : "notes-4w4" ou "cours"
+            wp_nav_menu(array(
+            "menu" => $menu,
+            "container" => "nav",
+    
+            )) ?>
+
+    </aside>
