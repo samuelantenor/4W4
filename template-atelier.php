d40eeee6633bd901667f4fdd035baedffab9f342
Samuel Antenor <asamuelandre@gmail.com>
2023-05-23 12:32:36 -0400

    rajout et stylisation du tableau

--- template-atelier.php
+++ template-atelier.php
@@ -10,13 +10,30 @@
 if ( have_posts() ) : the_post(); ?>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
-<p>Date <?php the_field('date_de_latelier'); ?></p>
-<p>Heure <?php the_field('heure_de_la_formation'); ?></p>
-<p>Durée <?php the_field('duree'); ?></p>
-<p>Local <?php the_field('local'); ?></p>
-<p>Formateur <?php the_field('formateur'); ?></p>
+<table>
+    <tr>
+        <th>Date</th>
+        <td><?php the_field('date_de_latelier'); ?></td>
+    </tr>
+    <tr>
+        <th>Heure</th>
+        <td><?php the_field('heure_de_la_formation'); ?></td>
+    </tr>
+    <tr>
+        <th>Durée</th>
+        <td><?php the_field('duree'); ?> heures</td>
+    </tr>
+    <tr>
+        <th>Local</th>
+        <td><?php the_field('local'); ?></td>
+    </tr>
+    <tr>
+        <th>Formateur</th>
+        <td><?php the_field('formateur'); ?></td>
+    </tr>
+</table>
     
 <?php endif;?>
 </main><!-- #main -->
 <?php
-get_footer();   
+get_footer();
